fa4faacd7be95a20caa11a300c54cbd6a4d19f9b
simonmader17 <mail@simonmader.at>
2024-02-24 22:26:51 +0100

    Added support for newer less versions

--- colored-man-pages.plugin.zsh
+++ colored-man-pages.plugin.zsh
@@ -11,14 +11,23 @@
 # ue       stop underline
 
 function man() {
-	env \
-		LESS_TERMCAP_md=$(tput bold; tput setaf 4) \
-		LESS_TERMCAP_me=$(tput sgr0) \
-		LESS_TERMCAP_mb=$(tput blink) \
-		LESS_TERMCAP_us=$(tput setaf 2) \
-		LESS_TERMCAP_ue=$(tput sgr0) \
-		LESS_TERMCAP_so=$(tput smso) \
-		LESS_TERMCAP_se=$(tput rmso) \
-		PAGER="${commands[less]:-$PAGER}" \
-		man "$@"
+	if less --help | grep -e "--use-color" &>/dev/null; then
+		# newer less versions
+		env \
+			MANPAGER="less -R --use-color -Dd+r -Du+b" \
+			MANROFFOPT="-P -c" \
+			man "$@"
+	else
+		# older less versions
+		env \
+			LESS_TERMCAP_md=$(tput bold; tput setaf 4) \
+			LESS_TERMCAP_me=$(tput sgr0) \
+			LESS_TERMCAP_mb=$(tput blink) \
+			LESS_TERMCAP_us=$(tput setaf 2) \
+			LESS_TERMCAP_ue=$(tput sgr0) \
+			LESS_TERMCAP_so=$(tput smso) \
+			LESS_TERMCAP_se=$(tput rmso) \
+			PAGER="${commands[less]:-$PAGER}" \
+			man "$@"
+	fi
 }
